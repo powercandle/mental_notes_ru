a1c551f5e40d630c58940b78bd8276fd4d823a0e
alex_ezhovv <aezhov4@gmail.com>
2022-08-13 23:15:46 +0800

    Initial Commit

---
+++ mental_notes_ru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mental_notes_ru
+//
+//  Created by Артём Рябенко on 13.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mental_notes_ru/mental_notes_ruApp.swift
@@ -0,0 +1,17 @@
+//
+//  mental_notes_ruApp.swift
+//  mental_notes_ru
+//
+//  Created by Артём Рябенко on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct mental_notes_ruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
